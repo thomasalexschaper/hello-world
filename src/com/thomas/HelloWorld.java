b45876cf23b450b5b86c2ca219229ae50cc5bc5b
Thomas Schaper <thomas.schaper@sharis.de>
2017-05-03 10:50:47 +0200

    Hello World extracted to a constant variable

--- src/com/thomas/HelloWorld.java
+++ src/com/thomas/HelloWorld.java
@@ -1,6 +1,8 @@
 public class HelloWorld { 
 
+	private static final String HELLO_WORLD = "Hello World!";
+
 	public static void main(String[] args) { 
-		System.out.println("Hello World!");
+		System.out.println(HELLO_WORLD);
 	}
 }
