23757048023c14f83f1b2c9595968b50460333db
Thomas Schaper <thomas.schaper@sharis.de>
2017-05-03 11:03:59 +0200

    Extend Hello World by a first country

--- src/com/thomas/HelloWorld.java
+++ src/com/thomas/HelloWorld.java
@@ -4,5 +4,15 @@ public class HelloWorld {
 
 	public static void main(String[] args) { 
 		System.out.println(HELLO_WORLD);
+		String[][] countries = new String[10][2];
+
+		countries[0][0] = "Germany";
+		countries[0][1] = "Europa";
+
+		writeOutCountry(countries[0][0], countries[0][1]);
+	}
+
+	private void writeOutCountry(String countryName, String continent) { 
+		System.out.println("The country is "+countryName+", and it is located in "+continent);
 	}
 }
